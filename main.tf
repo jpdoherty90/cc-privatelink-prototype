57487cae8a01f5f8cd896cd7d94ff28c655b034b
John Doherty <jpdoherty90@gmail.com>
2024-11-12 11:47:59 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = "2.9.0"
+    }
+  }
+}
+
+provider "confluent" {
+}
+
+resource "confluent_environment" "privatelink_prototype" {
+  display_name = "privatelink-prototype"
+
+  stream_governance {
+    package = "ESSENTIALS"
+  }
+}
+
+resource "confluent_kafka_cluster" "pl_prototype_cluster" {
+  display_name = "pl-prototype-cluster"
+  availability = "HIGH"
+  cloud        = "AWS"
+  region       = "us-east-2"
+  enterprise {}
+
+  environment {
+    id = confluent_environment.development.id
+  }
+}
