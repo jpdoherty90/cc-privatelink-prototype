986f0b2a3ee9ccbebb57c59c3cd3d431ca022789
John Doherty <jpdoherty90@gmail.com>
2024-11-15 16:23:21 -0500

    Adding PL resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,11 +4,19 @@ terraform {
       source  = "confluentinc/confluent"
       version = "2.9.0"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
 
 provider "confluent" {
 }
+provider "aws" {
+  region = "us-east-2"
+}
+
 
 resource "confluent_environment" "privatelink_prototype" {
   display_name = "privatelink-prototype"
@@ -18,7 +26,22 @@ resource "confluent_environment" "privatelink_prototype" {
   }
 }
 
+
+/*
+At a high level, the steps for setting up AWS PrivateLink connection to Confluent Cloud are:
+1. Create confluent_network resource with PRIVATELINK configuration
+2. Create a Dedicated kafka cluster with that network
+3. Create a VPC in AWS
+4. Add confluent_private_link_access resource
+5. Provision PrivateLink endpoints in AWS
+6. Set up DNS records in AWS
+7. Test PrivateLink connectivity to Confluent Cloud
+*/
+
+
+# STEP 1 Create confluent_network resource with PRIVATELINK configuration
 # Dedicated cluster w/ PrivateLink relies on confluent_network to exist
+# Note: AZ use1-az3 is not supported
 resource "confluent_network" "aws_private_link" {
   display_name     = "AWS Private Link Network"
   cloud            = "AWS"
@@ -30,6 +53,7 @@ resource "confluent_network" "aws_private_link" {
   }
 }
 
+# STEP 2: Create a Dedicated kafka cluster with that network
 resource "confluent_kafka_cluster" "pl_prototype_cluster" {
   display_name = "pl-prototype-cluster"
   availability = "MULTI_ZONE"
@@ -48,13 +72,29 @@ resource "confluent_kafka_cluster" "pl_prototype_cluster" {
   }
 }
 
+# STEP 3: Create a VPC in AWS 
+resource "aws_vpc" "pl_prototype_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
 
-/*
-The steps for setting up AWS PrivateLink connection to Confluent Cloud are:
-1. Create confluent_network resource with PRIVATELINK configuration (above)
-2. Add confluent_private_link_access resource
-3. Provision PrivateLink endpoints in AWS
-4. Set up DNS records in AWS
-5. Test PrivateLink connectivity to Confluent Cloud
-*/
+  tags = {
+    Name       = "pl-prototype-vpc"
+    created_by = "terraform"
+  }
+}
 
+
+# STEP 4: Add confluent_private_link_access resource
+resource "confluent_private_link_access" "aws" {
+  display_name = "AWS Private Link Access"
+  aws {
+    account = "<ADD AWS ACCOUNT ID>"
+  }
+  environment {
+    id = confluent_environment.privatelink_prototype.id
+  }
+  network {
+    id = confluent_network.aws_private_link.id
+  }
+
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "vpc_cidr" {
+  type        = string
+  description = "VPC IPv4 CIDR"
+  default     = "10.0.0.0/16"
+}
