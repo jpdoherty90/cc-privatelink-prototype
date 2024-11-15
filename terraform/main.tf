07f647f2a8f1132a5d5f803fc6666b37f7780ba6
John Doherty <jpdoherty90@gmail.com>
2024-11-15 15:39:22 -0500

    Added privatelink network

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,6 +18,18 @@ resource "confluent_environment" "privatelink_prototype" {
   }
 }
 
+# Dedicated cluster w/ PrivateLink relies on confluent_network to exist
+resource "confluent_network" "aws_private_link" {
+  display_name     = "AWS Private Link Network"
+  cloud            = "AWS"
+  region           = "us-east-2"
+  connection_types = ["PRIVATELINK"]
+  zones            = ["use2-az1", "use2-az2", "use2-az3"]
+  environment {
+    id = confluent_environment.development.id
+  }
+}
+
 resource "confluent_kafka_cluster" "pl_prototype_cluster" {
   display_name = "pl-prototype-cluster"
   availability = "MULTI_ZONE"
@@ -30,14 +42,19 @@ resource "confluent_kafka_cluster" "pl_prototype_cluster" {
   environment {
     id = confluent_environment.privatelink_prototype.id
   }
+
+  network {
+    id = confluent_network.aws_private_link.id
+  }
 }
 
 
 /*
 The steps for setting up AWS PrivateLink connection to Confluent Cloud are:
-1. Create confluent_network resource
+1. Create confluent_network resource with PRIVATELINK configuration (above)
 2. Add confluent_private_link_access resource
 3. Provision PrivateLink endpoints in AWS
 4. Set up DNS records in AWS
 5. Test PrivateLink connectivity to Confluent Cloud
 */
+
